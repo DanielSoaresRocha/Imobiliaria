9ff2b0b9b8394b5df240def87fc98e6be435f08f
WesleyLeocadio <silvawesley374@gmail.com>
2020-10-01 21:06:33 -0300

    #48 adicionando operação de instanciação

--- backend/src/main/java/com/backend/domain/Automovel.java
+++ backend/src/main/java/com/backend/domain/Automovel.java
@@ -23,8 +23,16 @@ public class Automovel extends Produto{
     private Double potenciaMotor;
     private String combustivel;
 
-
-
-
-
+    public Automovel(Integer id, String nome, Double valor, String descricao, String modelo, String cor, String quilometragem, String marca, Integer ano, Integer portas, String cambio, Double potenciaMotor, String combustivel) {
+        super(id, nome, valor, descricao);
+        this.modelo = modelo;
+        this.cor = cor;
+        this.quilometragem = quilometragem;
+        this.marca = marca;
+        this.ano = ano;
+        this.portas = portas;
+        this.cambio = cambio;
+        this.potenciaMotor = potenciaMotor;
+        this.combustivel = combustivel;
+    }
 }
