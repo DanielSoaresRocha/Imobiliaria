e6ba65de80deb396c4c024dea3a7a05ba7f9b1aa
DanielSoaresRocha <legoliga123@gmail.com>
2020-10-14 16:31:48 -0300

    #56 primeiro formulário da tela concluido

--- frontend/src/app/modules/advertiser/navbar-advertiser/navbar-advertiser.component.ts
+++ frontend/src/app/modules/advertiser/navbar-advertiser/navbar-advertiser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-advertiser',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarAdvertiserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  backToHome(){
+    console.log("hellou")
+    this.router.navigate(['/']);
   }
 
 }
