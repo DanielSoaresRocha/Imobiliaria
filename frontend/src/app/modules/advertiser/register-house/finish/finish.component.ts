89870f45a66121540ad8a07f830ffd5b60ef9608
DanielSoaresRocha <legoliga123@gmail.com>
2020-11-03 10:06:00 -0300

    #68 Mudando nomeclaturas

--- frontend/src/app/modules/advertiser/register-house/finish/finish.component.ts
+++ frontend/src/app/modules/advertiser/register-house/finish/finish.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-finish',
@@ -8,13 +9,14 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class FinishComponent implements OnInit {
   @Output() stageEmit = new EventEmitter();
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   finish(){
-    this.stageEmit.emit({stage: 5}); 
+    this.stageEmit.emit({stage: 5});
+    this.router.navigate(['/welcome']);
   }
   
   back(){
