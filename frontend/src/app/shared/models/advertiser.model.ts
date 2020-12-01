56972b8f6d3e0e8fef6fab5eac472e2240da19b0
DanielSoaresRocha <legoliga123@gmail.com>
2020-11-30 23:17:05 -0300

    #84 Conectando a API

---
+++ frontend/src/app/shared/models/advertiser.model.ts
@@ -0,0 +1,10 @@
+export class Advertiser{
+    nome: string
+    email: string
+    cpfOuCnpj: string
+    tipo: number = 1;
+    senha: string
+    telefone1: string
+    telefone2: string
+    telefone3: string
+}
--- frontend/src/app/shared/services/advertisement/advertiser/advertiser.service.ts
+++ frontend/src/app/shared/services/advertisement/advertiser/advertiser.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Advertiser } from '../../../models/advertiser.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdvertiserService {
-  private readonly BASE_URL = 'http://localhost:8080/login';
+  private readonly BASE_URL = 'http://localhost:8080';
 
   constructor(private httpClient: HttpClient) { }
 
   login(email, password): Observable<any>{
-    return this.httpClient.post(`${this.BASE_URL}`, {
+    return this.httpClient.post(`${this.BASE_URL}/login`, {
       email: email,
       senha: password
     });
+  }
+
+  register(advertiser: Advertiser): Observable<any>{
+    return this.httpClient.post(`${this.BASE_URL}/anunciantes`, advertiser);
   }
 }
