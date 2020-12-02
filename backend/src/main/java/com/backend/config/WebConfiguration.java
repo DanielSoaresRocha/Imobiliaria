50d8cab899448e5b1592833f73ef1bc40cf69db6
WesleyLeocadio <silvawesley374@gmail.com>
2020-12-01 22:29:22 -0300

    #91 Autorização

--- backend/src/main/java/com/backend/config/WebConfiguration.java
+++ backend/src/main/java/com/backend/config/WebConfiguration.java
@@ -37,16 +37,20 @@ public class WebConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/produtos/**",
-            "/anunciantes/**",
-            "/login/**"
+            "/anunciantes/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/anunciantes/**"
     };
 
 
-    //
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
