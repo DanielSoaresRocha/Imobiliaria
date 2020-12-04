77c7e127cc78fd5711769dee8853c5d4ae5a03a0
DanielSoaresRocha <legoliga123@gmail.com>
2020-12-04 16:03:59 -0300

    #92 Ao clicar em um card redireciona para detalhes da casa

--- frontend/src/app/modules/advertiser/adverts/adverts.component.ts
+++ frontend/src/app/modules/advertiser/adverts/adverts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { House } from 'src/app/shared/models/house.model';
 import { HouseService } from '../../../shared/services/advertisement/house'
 
@@ -10,13 +11,17 @@ import { HouseService } from '../../../shared/services/advertisement/house'
 export class AdvertsComponent implements OnInit {
   houses: House[];
 
-  constructor(private houseService: HouseService) { }
+  constructor(private houseService: HouseService, private router: Router) { }
 
   ngOnInit(): void {
     this.houseService.listByAdvertiser("2").subscribe(
       reponse => this.houses = reponse,
       error => alert('Ocorreu um erro')
     )
+  }
+
+  houseDetails(house: House): void {
+    this.router.navigate([`/advertisement/house-details`, house.id]);
   }
 
 }
